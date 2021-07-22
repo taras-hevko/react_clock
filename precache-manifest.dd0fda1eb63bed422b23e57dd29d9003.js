814e2e40886fa2130990b1819d508f2c7cd353b1
Taras <hevko.tars@gmail.com>
2021-07-22 19:46:19 +0300

    Updates

---
+++ precache-manifest.dd0fda1eb63bed422b23e57dd29d9003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51af239e89cb585fcfa6ccabf47673cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8bd96bb8c54285965e38",
+    "url": "./static/css/main.2f3411ad.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd96bb8c54285965e38",
+    "url": "./static/js/main.85455c0e.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
